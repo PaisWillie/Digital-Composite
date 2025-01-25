95def00caf15e985e73029e5bb6ebf84aa7a7ca9
Willie Pai <PaisWillie@gmail.com>
2025-01-24 19:48:56 -0500

    feat: install HeroUI pkg

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'components/App'
+import { HeroUIProvider } from '@heroui/react'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <HeroUIProvider>
+    <App />
+  </HeroUIProvider>
+)
--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
+import { heroui } from '@heroui/react'
+
 export default {
-  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{mjs,js,ts,jsx,tsx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {}
   },
-  plugins: []
+  darkMode: 'class',
+  plugins: [heroui()]
 }
