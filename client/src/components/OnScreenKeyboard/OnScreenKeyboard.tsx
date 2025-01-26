01d48e9f8e6c33fc05f56437c540332d8556f247
Willie Pai <PaisWillie@gmail.com>
2025-01-26 14:12:18 -0500

    feat: call action on key press

--- client/src/components/OnScreenKeyboard/OnScreenKeyboard.tsx
+++ client/src/components/OnScreenKeyboard/OnScreenKeyboard.tsx
@@ -1,85 +1,90 @@
 import { cn } from '@udecode/cn'
 
 type OnScreenKeyboardProps = {
-  onPress: (key: string) => void
+  onPress: (keyPressed: string) => void
 }
 
 const OnScreenKeyboard = ({ onPress }: OnScreenKeyboardProps) => {
+  const renderButton = (
+    label: React.ReactNode,
+    keyPressed: string,
+    additionalClasses: string = ''
+  ) => (
+    <button
+      className={cn([keyStyle, additionalClasses])}
+      onClick={() => onPress(keyPressed)}
+    >
+      {label}
+    </button>
+  )
+
   return (
     <div className="grid grid-rows-4">
       <div id="row-1" className="grid-cols-16 grid">
-        <button className={cn([keyStyle, 'col-span-2 aspect-auto'])}>
-          abc123
-        </button>
-        <button className={cn([keyStyle, leftBorderStyle])}>q</button>
-        <button className={keyStyle}>w</button>
-        <button className={keyStyle}>e</button>
-        <button className={keyStyle}>r</button>
-        <button className={keyStyle}>t</button>
-        <button className={keyStyle}>y</button>
-        <button className={keyStyle}>u</button>
-        <button className={keyStyle}>i</button>
-        <button className={keyStyle}>o</button>
-        <button className={keyStyle}>p</button>
-        <button className={cn([keyStyle, rightBorderStyle])}>backspace</button>
-        <button className={keyStyle}>1</button>
-        <button className={keyStyle}>2</button>
-        <button className={keyStyle}>3</button>
+        {renderButton('abc123', 'abc123', 'col-span-2 aspect-auto')}
+        {renderButton('q', 'q', leftBorderStyle)}
+        {renderButton('w', 'w')}
+        {renderButton('e', 'e')}
+        {renderButton('r', 'r')}
+        {renderButton('t', 't')}
+        {renderButton('y', 'y')}
+        {renderButton('u', 'u')}
+        {renderButton('i', 'i')}
+        {renderButton('o', 'o')}
+        {renderButton('p', 'p')}
+        {renderButton('backspace', 'backspace', rightBorderStyle)}
+        {renderButton('1', '1')}
+        {renderButton('2', '2')}
+        {renderButton('3', '3')}
       </div>
       <div id="row-2" className="grid-cols-16 grid">
-        <button className={cn([keyStyle, 'col-span-2 aspect-auto'])}>
-          @?#
-        </button>
-        <button className={cn([keyStyle, leftBorderStyle])}>a</button>
-        <button className={keyStyle}>s</button>
-        <button className={keyStyle}>d</button>
-        <button className={keyStyle}>f</button>
-        <button className={keyStyle}>g</button>
-        <button className={keyStyle}>h</button>
-        <button className={keyStyle}>j</button>
-        <button className={keyStyle}>k</button>
-        <button className={keyStyle}>l</button>
-        <button className={keyStyle}>;</button>
-        <button className={cn([keyStyle, rightBorderStyle])}>&apos;</button>
-        <button className={keyStyle}>4</button>
-        <button className={keyStyle}>5</button>
-        <button className={keyStyle}>6</button>
+        {renderButton('@?#', '@?#', 'col-span-2 aspect-auto')}
+        {renderButton('a', 'a', leftBorderStyle)}
+        {renderButton('s', 's')}
+        {renderButton('d', 'd')}
+        {renderButton('f', 'f')}
+        {renderButton('g', 'g')}
+        {renderButton('h', 'h')}
+        {renderButton('j', 'j')}
+        {renderButton('k', 'k')}
+        {renderButton('l', 'l')}
+        {renderButton(';', ';')}
+        {renderButton("'", "'", rightBorderStyle)}
+        {renderButton('4', '4')}
+        {renderButton('5', '5')}
+        {renderButton('6', '6')}
       </div>
       <div id="row-3" className="grid-cols-16 grid">
         <div className={cn([keyStyle, 'col-span-2 aspect-auto'])} />
-        <button className={cn([keyStyle, leftBorderStyle])}>shift</button>
-        <button className={keyStyle}>z</button>
-        <button className={keyStyle}>x</button>
-        <button className={keyStyle}>c</button>
-        <button className={keyStyle}>v</button>
-        <button className={keyStyle}>b</button>
-        <button className={keyStyle}>n</button>
-        <button className={keyStyle}>m</button>
-        <button className={keyStyle}>,</button>
-        <button className={keyStyle}>.</button>
-        <button className={cn([keyStyle, rightBorderStyle])}>/</button>
-        <button className={keyStyle}>7</button>
-        <button className={keyStyle}>8</button>
-        <button className={keyStyle}>9</button>
+        {renderButton('shift', 'shift', leftBorderStyle)}
+        {renderButton('z', 'z')}
+        {renderButton('x', 'x')}
+        {renderButton('c', 'c')}
+        {renderButton('v', 'v')}
+        {renderButton('b', 'b')}
+        {renderButton('n', 'n')}
+        {renderButton('m', 'm')}
+        {renderButton(',', ',')}
+        {renderButton('.', '.')}
+        {renderButton('/', '/', rightBorderStyle)}
+        {renderButton('7', '7')}
+        {renderButton('8', '8')}
+        {renderButton('9', '9')}
       </div>
       <div id="row-4" className="grid-cols-16 grid">
-        <button className={cn([keyStyle, 'col-span-2 aspect-auto'])}>
-          return
-        </button>
-        <button className={cn([keyStyle, leftBorderStyle])}>left</button>
-        <button className={keyStyle}>right</button>
+        {renderButton('return', 'return', 'col-span-2 aspect-auto')}
+        {renderButton('left', 'left', leftBorderStyle)}
+        {renderButton('right', 'right')}
         <div className={keyStyle} />
-        <button className={cn([keyStyle, 'col-span-5 aspect-auto'])}>
-          space
-        </button>
+        {renderButton('space', 'space', 'col-span-5 aspect-auto')}
         <div className={keyStyle} />
-        <button
-          className={cn([keyStyle, rightBorderStyle, 'col-span-2 aspect-auto'])}
-        >
-          search
-        </button>
+        {renderButton(
+          'search',
+          'search',
+          `${rightBorderStyle} col-span-2 aspect-auto`
+        )}
         <div className={keyStyle} />
-        <button className={keyStyle}>0</button>
+        {renderButton('0', '0')}
         <div className={keyStyle} />
       </div>
     </div>
