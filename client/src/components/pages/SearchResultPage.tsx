3c87c79b358e96ecc2eba84f3b8720215736545c
Willie Pai <PaisWillie@gmail.com>
2025-03-29 13:05:13 -0400

    fix: linting

--- client/src/components/pages/SearchResultPage.tsx
+++ client/src/components/pages/SearchResultPage.tsx
@@ -227,7 +227,7 @@ const SearchResultPage = () => {
               students={filteredComposites[selectedCompositeId].students || []}
               onStudentClick={onStudentClick}
             />
-            <p className="font-sm flex w-full flex-row justify-center italic text-gray-500">
+            <p className="flex w-full flex-row justify-center italic text-gray-500">
               Need your photo removed? Contact
               <span className="ml-1 underline">alumni@mcmaster.ca</span>
             </p>
