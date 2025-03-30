73693866367a6e307341035f4c7e58158ef98584
Willie Pai <PaisWillie@gmail.com>
2025-03-29 22:45:40 -0400

    fix: text wrap on large screens

--- client/src/components/pages/SearchResultPage.tsx
+++ client/src/components/pages/SearchResultPage.tsx
@@ -246,7 +246,7 @@ const SearchResultPage = () => {
                   Back
                 </TextButton>
               </div>
-              <p className="flex w-full flex-row justify-center italic text-gray-500 font-poppins mb-4">
+              <p className="flex w-full flex-row justify-center italic text-gray-500 font-poppins mb-4 xl:text-sm">
                 Need your photo removed? Contact
                 <span className="ml-1 underline">engalum@mcmaster.ca</span>
               </p>
